c1d7e9cef91cbca2513d92556ff7db75aafb73ab
ajaykumargadireddy <69164978+ajaykumargadireddy@users.noreply.github.com>
2021-07-22 13:21:57 +0530

    Add files via upload

---
+++ DoubleLinkedList/DobMain.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtusaDay11
+{
+    class DobMain
+    {
+        static void Main(string[] args)
+        {
+            DoubledLinkedlist d = new DoubledLinkedlist();
+            
+            int ch,ele,pos,res,oc;
+            do
+            {
+                Console.WriteLine("1. Insert at begin");
+                Console.WriteLine("2. Insert at end");
+                Console.WriteLine("3. Insert at position");
+                Console.WriteLine("4. Delete at begin");
+                Console.WriteLine("5. Delete at end");
+                Console.WriteLine("6. Delete at Position");
+                Console.WriteLine("7. Find an element");
+                Console.WriteLine("8. Find an element after a Occurence");
+                Console.WriteLine("9. Display");
+                Console.WriteLine("10. exit");
+                Console.WriteLine("11. Traverse Reverse from given Position");
+                Console.WriteLine("Enter your choice");
+                ch = int.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1: Console.WriteLine("Enter your element");
+                        ele = int.Parse(Console.ReadLine());
+                        d.insertbegin(ele);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter your element");
+                        ele = int.Parse(Console.ReadLine());
+                        d.insertend(ele);
+                        break;
+                    case 3: Console.WriteLine("Enter your element");
+                            ele= int.Parse(Console.ReadLine());
+                        do
+                        {
+                            Console.WriteLine("Enter Position");
+                            pos = int.Parse(Console.ReadLine());
+                        } while (pos < 1 || pos > d.count + 1);
+                        d.insertatpos(ele, pos);
+                        break;
+                    case 4:d.deletebegin();
+                        break;
+                    case 5:
+                        d.deleteend();
+                        break;
+                    case 6:
+                        do
+                        {
+                            Console.WriteLine("Enter Position");
+                            pos = int.Parse(Console.ReadLine());
+                        } while (pos < 1 || pos > d.count);
+                        d.deleteatpos(pos);
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter your element");
+                        ele = int.Parse(Console.ReadLine());
+                        res=d.find(ele);
+                        if (res == -1)
+                            Console.WriteLine("Element is not found");
+                        else
+                            Console.WriteLine("The Key is found at position:" + res);
+                        break;
+                    case 8:
+                        Console.WriteLine("Enter your element");
+                        ele = int.Parse(Console.ReadLine());
+                        do
+                        {
+                            Console.WriteLine("Enter occurence number");
+                            oc = int.Parse(Console.ReadLine());
+                            
+                        } while (oc < 1 || oc > d.count);
+                        res = d.find(ele, oc);
+                        if (res == -1)
+                            Console.WriteLine("Element is not found");
+                        else if(res!=0)
+                            Console.WriteLine("The Key is found at :" + res);
+                        break;
+                    case 9:d.display();
+                        break;
+                    case 11:
+                        do
+                        {
+                            Console.WriteLine("Enter Position");
+                            pos = int.Parse(Console.ReadLine());
+                        } while (pos < 1 || pos > d.count);
+                        d.Traversereverse(pos);
+                        break;
+                    case 10:break;
+                }
+            } while (ch != 10);
+
+        }
+    }
+}
---
+++ DoubleLinkedList/DoubledLinkedlist.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtusaDay11
+{
+    public class DNode
+    {
+        public int data;
+        public DNode prev;
+        public DNode next;
+        public DNode(int data)
+        {
+            this.next = null;
+            this.prev = null;
+            this.data = data;
+        }
+        public void display()
+        {
+            Console.Write(data + "\t");
+        }
+        public override string ToString()
+        {
+            return ("\nThe deleted Node is " + data+"\n");
+        }
+    }
+    public class DoubledLinkedlist
+    {
+        public DNode head = null;
+        public int count = 0;
+        public void insertbegin(int ele)
+        {
+            if (head == null)
+                head = createNode(ele);
+            else
+            {
+                DNode temp = createNode(ele);
+                temp.next = head;
+                head.prev = temp;
+                head = temp;
+            }
+        }
+
+        public DNode createNode(int ele)
+        {
+            count++;
+            return new DNode(ele);
+        }
+
+        public void insertend(int ele)
+        {
+            if (head == null)
+                head = createNode(ele);
+            else
+            {
+                DNode temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                DNode newnode= createNode(ele);
+                temp.next = newnode;
+                newnode.prev = temp;
+            }
+        }
+
+        public void display()
+        {
+            DNode temp = head;
+            Console.WriteLine();
+            Console.WriteLine();
+            if (head == null)
+                Console.WriteLine("The List is Empty");
+            else
+            {
+                while (temp != null)
+                {
+                    temp.display();
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public void insertatpos(int ele, int pos)
+        {
+            if (pos == 1)
+                insertbegin(ele);
+            else if (pos == count + 1)
+                insertend(ele);
+            else
+            {
+                DNode cn = head, pn = head;
+                for(int i = 1; i < pos; i++)
+                {
+                    pn = cn;
+                    cn = cn.next;
+                }
+                DNode newnode = createNode(ele);
+                pn.next = newnode;
+                newnode.next = cn;
+                newnode.prev = pn;
+                cn.prev = newnode;
+            }
+        }
+
+        public void deleteatpos(int pos)
+        {
+            if (pos == 1)
+                deletebegin();
+            else if (pos == count)
+                deleteend();
+            else
+            {
+                DNode pn = head, cn = head;
+                for(int i = 1; i < pos; i++)
+                {
+                    pn = cn;
+                    cn = cn.next;
+                }
+                pn.next = cn.next;
+                if (pn.next != null)
+                    pn.next.prev = pn;
+                Console.WriteLine(cn);
+                cn = null;
+            }
+        }
+
+        internal int find(int ele,int cnt=1)
+        {
+            DNode temp = head;
+            int i = 0,pos=0;
+            while (temp != null)
+            {
+                if (temp.data == ele)
+                {
+                    i += 1;  
+                    if(i==cnt)
+                        return pos + 1;
+                }
+                temp = temp.next;
+                pos++;
+            }
+            if (i > 0)
+            {
+                Console.WriteLine("Occuernce Not found");
+                return 0;
+            }
+            return -1;
+        }
+
+        public void deleteend()
+        {
+            if (head == null)
+                Console.WriteLine("Deletion is Not Possible");
+            else
+            {
+                DNode pn = head, cn = head;
+                while (cn.next != null)
+                {
+                    pn = cn;
+                    cn = cn.next;
+                }
+                pn.next = cn.next;
+                Console.WriteLine(cn);
+                if (pn == cn)
+                    head = null;
+                cn = null;
+                count--;
+            }
+        }
+
+        internal void Traversereverse(int pos)
+        {
+            DNode temp = head;
+            for (int i = 1; i < pos; i++)
+                temp = temp.next;
+            while (temp != null)
+            {
+                temp.display();
+                temp = temp.prev;
+            }
+            Console.WriteLine();
+        }
+
+        public void deletebegin()
+        {
+            if (head == null)
+                Console.WriteLine("Deletion is Not Possible");
+            else
+            {
+                DNode temp = head;
+                head = head.next;
+                if (head != null)
+                    head.prev = null;
+                Console.WriteLine(temp);
+                temp = null;
+                count--;
+            }
+        }
+    }
+}
